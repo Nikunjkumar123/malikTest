3ce57eb5ad3c452b1387bfe550aa495a63681a1d
nikunj kumar <nikunjkumar2911@gmail.com>
2025-01-31 12:50:25 +0530

    update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const freekaViewroute = require('./Routers/freekaViewroute.js');
 const ProfilesRouter = require('./Routers/ProfilesRouter.js');
 
 const corsOptions = {
-    origin: 'http://localhost:3001', 
+    origin: 'https://y-lyart-one-86.vercel.app', 
     methods: 'GET, POST, PUT, DELETE,PATCH', 
     credentials: true, 
 };
@@ -28,7 +28,7 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(fileUpload({useTempFiles:true}));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // your frontend URL
+    res.header('Access-Control-Allow-Origin', 'https://y-lyart-one-86.vercel.app'); // your frontend URL
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
